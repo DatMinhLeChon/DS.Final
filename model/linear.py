a23555e7c294af162bc44f8bb096db36dd46174d
Dat <dat.lemindast@gmail.com>
2023-06-12 14:21:35 +0700

    change

--- model/linear.py
+++ model/linear.py
@@ -4,10 +4,9 @@ import pandas as pd
 from torch import nn 
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
-features = ['price',\
-        'city',\
-        'city_target_encoder',\
+features = ['city_target_encoder',\
         'city_index',\
         'area',\
         'new_num_floors',\
@@ -19,10 +18,23 @@ features = ['price',\
         'houseTypes_Bán Nhà riêng']
 
 df = pd.read_excel('final_data.xlsx')
+
+class splitData():
+    def __init__(self):
+        self.x_train = []
+        self.y_train = []
+        self.y_test = []
+        self.x_test = []
+        
+    def split(self, x, y):
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, y, test_size=0.1, random_state=42)
+    
 class ReSystem():
     def __init__(self):
         
-        return 
+        return
+    
+
 
 class LinearModel():
     def __init__(self):
@@ -34,5 +46,12 @@ class LinearModel():
         return reg
 if __name__ =="__main__":
     model = LinearModel()
-    model._X = df['price']
-    model._Y = 
+    
+    x = df['price'].values
+    y = df[features].values
+    data = splitData()
+    data.split(x, y)
+    model._X = data.x_train
+    model._y = data.y_train
+    
+    print(model.linear())
